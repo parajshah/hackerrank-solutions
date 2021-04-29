28ce4fbd7ee16a5e4b76e1aaca3e5e435ed27a87
Paraj Shah <parajshah2000@gmail.com>
2021-04-30 00:11:22 +0530

    PS-Algo-easy-50

---
+++ Problem Solving/Algorithms/Easy/Cut the sticks.py
@@ -0,0 +1,9 @@
+from collections import deque
+
+def cutTheSticks(arr):
+    ls = []
+    while len(arr) >= 1:
+        ls.append(len(arr))
+        minimum = min(arr)
+        arr = [i - minimum for i in arr if i != minimum]
+    return ls
---
+++ Problem Solving/Algorithms/Easy/Two Strings.py
@@ -0,0 +1,2 @@
+def twoStrings(s1, s2):
+    return "YES" if set(s1) & set(s2) else "NO"
