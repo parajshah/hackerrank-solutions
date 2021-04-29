59a67faddf88e57f2679b54e7c6171d47f9f6e2b
Paraj Shah <parajshah2000@gmail.com>
2021-04-29 22:59:14 +0530

    PS-Algo-easy-20

---
+++ Problem Solving/Algorithms/Easy/Divisible Sum Pairs.py
@@ -0,0 +1,10 @@
+def divisibleSumPairs(n, k, a):
+    count = 0
+    for i in range(len(a)):
+        SUM = 0
+        for j in range(i+1,len(a)):
+            if i < j:
+                SUM = a[i] + a[j]
+                if SUM % k == 0:
+                    count += 1
+    return count
---
+++ Problem Solving/Algorithms/Easy/Number Line Jumps.py
@@ -0,0 +1,9 @@
+def kangaroo(x1, v1, x2, v2):
+    if x2 > x1 and v2 > v1:
+        return "NO"
+    for _ in range(10000):
+        x1 += v1
+        x2 += v2
+        if x1 == x2:
+            return "YES" 
+    return "NO"
