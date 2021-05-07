a21e794746ccc4cb912e8e857c01a8b7c1f4cf47
Paraj Shah <parajshah2000@gmail.com>
2021-05-07 18:14:07 +0530

    PS-DS-easy-16

---
+++ Problem Solving/Data Structures/Easy/Delete duplicate-value nodes from a sorted linked list.py
@@ -0,0 +1,8 @@
+def removeDuplicates(head):
+    cur = head
+    while cur.next:
+        if cur.data == cur.next.data:
+            cur.next = cur.next.next
+        else:
+            cur = cur.next
+    return head
---
+++ Problem Solving/Data Structures/Easy/Get Node Value.py
@@ -0,0 +1,12 @@
+def getNode(head, positionFromTail):
+    i = 0
+    cur = head
+    result = head
+    
+    while cur:
+        cur = cur.next
+        if i > positionFromTail:
+            result = result.next
+        i += 1
+        
+    return result.data
