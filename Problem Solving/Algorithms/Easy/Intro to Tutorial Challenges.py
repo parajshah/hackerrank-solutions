f30650731987859aa75332c647eeba007a8d2e68
Paraj Shah <parajshah2000@gmail.com>
2021-05-07 20:38:42 +0530

    PS-Algo-easy-67

---
+++ Problem Solving/Algorithms/Easy/Intro to Tutorial Challenges.py
@@ -0,0 +1,4 @@
+def introTutorial(V, arr):
+    for i in range(len(arr)):
+        if arr[i] == V:
+            return i
---
+++ Problem Solving/Algorithms/Easy/Running Time of Algorithms.py
@@ -0,0 +1,11 @@
+def runningTime(arr):
+    count = 0
+    for i in range(1, len(arr)):
+        element = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > element:
+            arr[j + 1] = arr[j]
+            j -= 1
+            count += 1
+        arr[j + 1] = element
+    return count
